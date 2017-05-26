384cf6fa12dbd44ccf258048b36519109c3fd445
kube-master2 <kube-master2@kube-master2.kubernetes>
2017-05-27 00:27:32 +0700

    Complete function

--- get_stdout.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import json
-
-batcmd="./telegram-dev"
-# result = subprocess.check_output(batcmd, shell=True)
-log = open("telegram-dev.log", "a")
-subprocess.Popen(batcmd, stdout=log, stderr=log, shell=True)
