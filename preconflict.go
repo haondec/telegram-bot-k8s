8bcced5539acb848ce0016e4171cb0b34cadea54
King <menly9x@gmail.com>
2017-07-03 23:48:32 +0700

    Update preconflict.go

--- preconflict.go
+++ preconflict.go
@@ -4,10 +4,39 @@ import (
         "errors"
         "fmt"
         "os"
+        "strings"
         "io/ioutil"
         "time"
 )
 
-func check_lock(fn string) {
-        
+const (
+        flock string = ".lock"        
+)
+
+func check_lock(fn string) bool {
+        fInfo, err := os.Stat(fn)
+        if err == nil {
+                if fInfo.IsDir() == false {
+                        return true
+                }
+        }
+        return false
+}
+
+func make_lock(path string) {
+        if strings.Contains(path, flock) == false {
+                fn := validatePath(path) + flock
+        }
+        if check_lock(fn) == false {
+                f, err := os.Create(fn)
+        }
+}
+
+func un_lock(path string) {
+        if strings.Contains(path, flock) == false {
+                fn := validatePath(path) + flock
+        }
+        if check_lock(fn) == false {
+                f, err := os.Remove(fn)
+        }
 }
