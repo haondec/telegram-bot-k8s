68199936563ae6050e86a5e95d4fd8d1b6f2f334
King <menly9x@gmail.com>
2017-07-04 08:44:51 +0700

    Update preconflict.go

--- preconflict.go
+++ preconflict.go
@@ -20,9 +20,30 @@ func check_lock(fn string) bool {
         return false
 }
 
+func subCL_getLast(path string, sep string) string {
+        s_split := strings.Split(path, sep)
+	// Always s_split have at least an member.
+	return s_split[len(s_split) - 1]
+}
+
+func check_lock_v2(path string) bool {
+        fn := subCL_getLast(path, "/")
+        if fn != flock {
+                fn = validatePath(path) + flock
+        }
+        fInfo, err := os.Stat(fn)
+        if err == nil {
+                if fInfo.IsDir() == false {
+                        return true
+                }
+        }
+        return false
+}
+
 func make_lock(path string) {
-        if strings.Contains(path, flock) == false {
-                fn := validatePath(path) + flock
+        fn := subCL_getLast(path, "/")
+        if fn != flock {
+                fn = validatePath(path) + flock
         }
         if check_lock(fn) == false {
                 f, err := os.Create(fn)
@@ -30,8 +51,9 @@ func make_lock(path string) {
 }
 
 func un_lock(path string) {
-        if strings.Contains(path, flock) == false {
-                fn := validatePath(path) + flock
+        fn := subCL_getLast(path, "/")
+        if fn != flock {
+                fn = validatePath(path) + flock
         }
         if check_lock(fn) == false {
                 f, err := os.Remove(fn)
